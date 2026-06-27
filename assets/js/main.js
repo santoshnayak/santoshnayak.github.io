beaca5a585ab584245ce1d39c3f813a3b56e1ae9
Santosh Nayak <santoshnayak1996@gmail.com>
2026-06-27 23:14:19 +0530

    new version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
-$(document).ready(function() {
-  AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true  
-  }); // initialize animate on scroll library
-});
+// AOS initialization moved to Angular components to support dynamic rendering.
+// $(document).ready(function() {
+//   AOS.init( {
+//     // uncomment below for on-scroll animations to played only once
+//     // once: true  
+//   }); // initialize animate on scroll library
+// });
 
 // Smooth scroll for links with hashes
 $('a.smooth-scroll')
